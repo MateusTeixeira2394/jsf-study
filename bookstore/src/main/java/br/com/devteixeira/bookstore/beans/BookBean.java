eee8979552979c1e2e85542af37b697477ff9455
Mateus Teixeira <mateus.teixeira2394@gmail.com>
2023-08-07 21:50:09 -0300

    Added the validations in book's form

--- bookstore/src/main/java/br/com/devteixeira/bookstore/beans/BookBean.java
+++ bookstore/src/main/java/br/com/devteixeira/bookstore/beans/BookBean.java
@@ -1,9 +1,16 @@
 package br.com.devteixeira.bookstore.beans;
 
+import java.math.BigDecimal;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
 
 import br.com.devteixeira.bookstore.models.Author;
 import br.com.devteixeira.bookstore.models.Book;
@@ -51,6 +58,42 @@ public class BookBean {
 		
 	}
 	
+	public void mustBePositiveNumberValidator(FacesContext fc, UIComponent component, Object inputValue) throws ValidatorException {
+		
+		try {
+			
+			BigDecimal price = new BigDecimal(inputValue.toString());
+			
+			if(price.compareTo(BigDecimal.ZERO) < 0) {
+				throw new ValidatorException(new FacesMessage("The price must be a positive number")); 
+			}
+			
+			if(price.compareTo(BigDecimal.ZERO) == 0) {
+				throw new ValidatorException(new FacesMessage("The price cant' be zero")); 
+			}
+			
+		} catch(NumberFormatException e) {
+			
+			throw new ValidatorException(new FacesMessage("The price must be numeric")); 
+			
+		}
+		
+	}
+	
+	public void mustBeValidDateValidator(FacesContext fc, UIComponent component, Object inputValue) throws ValidatorException {
+		
+		final String pattern = "dd/MM/yyyy";		
+		SimpleDateFormat dateFormat = new SimpleDateFormat(pattern);
+		
+		try {
+			
+			dateFormat.parse(inputValue.toString());
+			
+		} catch (ParseException e) {
+			throw new ValidatorException(new FacesMessage("The date release date must follow the dd/mm/yyyy format"));
+		}
+		
+	}
 	
 	public Long getAuthorId() {
 		return authorId;
